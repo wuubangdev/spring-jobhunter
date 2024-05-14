cde96f740d1abd42805d6f188dca0f84afda378f
BangLV <wuubangdev@gmail.com>
2024-05-14 16:13:59 +0700

    feat: create company domain, add lombok, format Iso format time

---
+++ src/main/java/vn/hoidanit/jobhunter/domain/Company.java
@@ -0,0 +1,36 @@
+package vn.hoidanit.jobhunter.domain;
+
+import java.time.Instant;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "companies")
+@Getter
+@Setter
+public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+
+    @Column(columnDefinition = "MEDIUMTEXT")
+    private String description;
+    private String address;
+    private String logo;
+
+    private Instant createdAt;
+    private Instant updateAt;
+
+    private String createBy;
+    private String updateBy;
+
+}
