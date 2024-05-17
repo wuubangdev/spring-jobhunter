e10e7882bbfcc949f34038a889f09d4ebb02302e
BangLV <wuubangdev@gmail.com>
2024-05-17 08:25:11 +0700

    feat: paginate user

--- src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
@@ -2,17 +2,21 @@ package vn.hoidanit.jobhunter.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import vn.hoidanit.jobhunter.domain.ResultPaginate;
 import vn.hoidanit.jobhunter.domain.User;
 import vn.hoidanit.jobhunter.service.UserService;
 import vn.hoidanit.jobhunter.service.error.IdInvalidException;
 
 import java.util.List;
 
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -42,9 +46,13 @@ public class UserController {
     }
 
     @GetMapping("/user")
-    public ResponseEntity<List<User>> getAllUser() {
-        List<User> users = this.userService.fetchAllUsers();
-        return ResponseEntity.ok(users);
+    public ResponseEntity<ResultPaginate> getAllUser(
+            @RequestParam("current") String sCurrent,
+            @RequestParam("pageSize") String sPageSize) {
+        Integer current = Integer.parseInt(sCurrent);
+        Integer pageSize = Integer.parseInt(sPageSize);
+        Pageable pageable = PageRequest.of(current - 1, pageSize);
+        return ResponseEntity.ok(this.userService.fetchAllUsers(pageable));
     }
 
     @PutMapping("/user")
--- src/main/java/vn/hoidanit/jobhunter/service/UserService.java
+++ src/main/java/vn/hoidanit/jobhunter/service/UserService.java
@@ -3,8 +3,13 @@ package vn.hoidanit.jobhunter.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import vn.hoidanit.jobhunter.domain.Company;
+import vn.hoidanit.jobhunter.domain.MetaDTO;
+import vn.hoidanit.jobhunter.domain.ResultPaginate;
 import vn.hoidanit.jobhunter.domain.User;
 import vn.hoidanit.jobhunter.repository.UserRepository;
 
@@ -28,8 +33,17 @@ public class UserService {
         return null;
     }
 
-    public List<User> fetchAllUsers() {
-        return this.userRepository.findAll();
+    public ResultPaginate fetchAllUsers(Pageable pageable) {
+        Page<User> userCompanies = this.userRepository.findAll(pageable);
+        MetaDTO mt = new MetaDTO();
+        ResultPaginate rsp = new ResultPaginate();
+        mt.setCurrent(userCompanies.getNumber());
+        mt.setPageSize(userCompanies.getSize());
+        mt.setTotalPages(userCompanies.getTotalPages());
+        mt.setTotalIteams(userCompanies.getTotalElements());
+        rsp.setMeta(mt);
+        rsp.setData(userCompanies.getContent());
+        return rsp;
     }
 
     public User updateUser(User user) {
