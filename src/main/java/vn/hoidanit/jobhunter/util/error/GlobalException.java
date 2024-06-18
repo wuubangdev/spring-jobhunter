e4556d8cbf2b38ab4b4a98d8143e2dd89418b40a
BangLV <wuubangdev@gmail.com>
2024-06-18 10:08:06 +0700

    global exception

--- src/main/java/vn/hoidanit/jobhunter/util/error/GlobalException.java
+++ src/main/java/vn/hoidanit/jobhunter/util/error/GlobalException.java
@@ -81,4 +81,13 @@ public class GlobalException {
         res.setMessage(ex.getMessage());
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(res);
     }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<RestResponse<Object>> handleAllException(Exception ex) {
+        RestResponse<Object> res = new RestResponse<Object>();
+        res.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        res.setMessage(ex.getMessage());
+        res.setError("Internal Server Error");
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(res);
+    }
 }
